ac2fd4e0e6502a3390ebfcd2c9a7060abeac2dc3
unknown <allan.rodrigues14@hotmail.com>
2024-05-27 21:59:55 -0300

    Creating Struct Project and primary route

---
+++ src/main/java/br/com/allandev/crud_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,16 @@
+package br.com.allandev.crud_vagas.modules.candidate;
+
+import java.util.UUID;
+
+import lombok.Data;
+
+@Data
+public class CandidateEntity {
+    private UUID uuid;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private String description;
+    private String curriculum;
+}
---
+++ src/test/java/br/com/allandev/crud_vagas/CrudVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.allandev.crud_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
